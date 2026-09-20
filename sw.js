f7a8b41f42d3e89008f8a08d96d24f3705e3965a
Abdul-Latif Ahmed <alatipha@ymail.com>
2026-09-20 22:59:45 +0000

    Update CACHE_VERSION to v1.0.15

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    get a "New version available" prompt on next visit.
 ===================================================== */
 
-const CACHE_VERSION = "v1.0.14a";
+const CACHE_VERSION = "v1.0.15";
 const CACHE_NAME    = `alatipha-ges-pasco-${CACHE_VERSION}`;
 
 /* ====================
