1e53e6682943bf43e68ffbde67c4f411fb294201
Abdul-Latif Ahmed <alatipha@ymail.com>
2026-09-20 22:10:17 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    get a "New version available" prompt on next visit.
 ===================================================== */
 
-const CACHE_VERSION = "v1.0.10";
+const CACHE_VERSION = "v1.0.11";
 const CACHE_NAME    = `alatipha-ges-pasco-${CACHE_VERSION}`;
 
 /* ====================
